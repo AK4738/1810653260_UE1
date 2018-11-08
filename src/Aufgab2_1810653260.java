72c5dd4a5e30f5869e7fc4230642f5c696d22611
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-08 15:40:41 +0100

    werte zuordnen

--- src/Aufgab2_1810653260.java
+++ src/Aufgab2_1810653260.java
@@ -2,6 +2,16 @@ public class Aufgab2_1810653260
 {
     public static void main (String[] args)
     {
+        String name = "Kadir1!";// string kann alle zeichen haben
+        int name = 25; //int kann nur ganze zahlen haben 32bit
+        double name = 3.84; //double kann ganze zahlen mit gleitkommerdarstellung haben 64 bit
+        boolean name = true; // boolean kann nur true oder false haben
+        long name = 20l;// long kann hinter den zahlen buchstaben haben und trotzdem rechenoperationen durchführen 64bit
+        char name='abc';//in einfachen anführungsstrichen 16bit
+        byte name = -12345678; // 8bit
+        short name = 1234; // 16bit
+        float name = 2.8f;// ist ähnlich wie double kann aber gleitkommerdarstellung haben 32bit
+
 
     }
 }
