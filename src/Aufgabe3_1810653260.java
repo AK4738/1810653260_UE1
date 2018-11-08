c433ebb0a80363fd9fe5a38a7d90d65d521c8cf4
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-08 15:54:45 +0100

    wert für int und double

--- src/Aufgabe3_1810653260.java
+++ src/Aufgabe3_1810653260.java
@@ -2,12 +2,12 @@ public class Aufgabe3_1810653260
 {
     public static void main (String[] args)
     {
-        int zahl1;// ohne komma
-        int zahl2;
-        int zahl3;
+        int zahl1 = 5;// ohne komma
+        int zahl2 = -6;
+        int zahl3 = 8;
 
-        double zahl4=2;// mit komma
-        double zahl5;
-        double zahl6;
+        double zahl4=2.5;// mit komma
+        double zahl5=1.5;
+        double zahl6=6.5;
     }
 }
