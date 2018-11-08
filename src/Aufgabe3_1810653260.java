7692abb91ca7231375a53c972feeeb0b3cd21a08
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-08 15:45:01 +0100

    Variablen mit int und double erzeugen

---
+++ src/Aufgabe3_1810653260.java
@@ -0,0 +1,13 @@
+public class Aufgabe3_1810653260
+{
+    public static void main (String[] args)
+    {
+        int zahl1;// ohne komma
+        int zahl2;
+        int zahl3;
+
+        double zahl4;// mit komma
+        double zahl5;
+        double zahl6;
+    }
+}
