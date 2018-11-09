d56b617093af30ba39bc98dc661f4380d11bb686
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-10 00:42:00 +0100

    kommentieren und die vorherigen 3 sind falsch

--- src/Aufgabe5_1810653260.java
+++ src/Aufgabe5_1810653260.java
@@ -5,6 +5,8 @@ public class Aufgabe5_1810653260
         int zahl1 = 1;
         int zahl2 = 2;
 
+        System.out.println("" + ++zahl1 + "\n" + ++zahl2);
         System.out.println("" + --zahl1 + "\n" + --zahl2);
+        /* zahl1++ erhöht die Variable nach abruf und ++zahl1 erhöht die Variable vor abruf */
     }
 }
