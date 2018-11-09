b7b7d4550619abf5e3b45618ce2c594e4f7d17af
mrkadir69 <1810653260@stud.fh-kufstein.ac.at>
2018-11-10 00:19:32 +0100

    erg8

--- src/Aufgabe4_1810653260.java
+++ src/Aufgabe4_1810653260.java
@@ -12,9 +12,11 @@ public class Aufgabe4_1810653260
         boolean erg5 = zahl1 <= zahl2;
         boolean erg6 = zahl2 <= zahl1;
         boolean erg7 = zahl1 >= zahl2;
+        boolean erg8 = zahl2 >= zahl1;
+
 
         System.out.println(""+ erg1 + "\n" + erg2 + "\n" +
                 erg3 + "\n" + erg4 + "\n" + erg5 + "\n" + erg6 +
-                "\n" + erg7);
+                "\n" + erg7 + "\n" + erg8);
     }
 }
